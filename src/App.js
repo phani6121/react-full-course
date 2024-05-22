4a3a81c525235db445b92920b702bb0d5e25d7f7
PHANINDRA KUMAR NIDAMANURI <git config --global user.email>
2024-05-22 16:11:46 +0530

    useReducer

--- src/App.js
+++ src/App.js
@@ -26,7 +26,9 @@ import React from "react";
 // import Index from "./useEffect/useEffect-example-1";
 // import Index from "./useEffect/useEffect-example-2";
 // import Index from "./useContext/useContextDemo";
-import Index from "./useContext/parentComponent";
+// import Index from "./useContext/parentComponent";
+// import Index from "./useReducer/useReducer-example-1";
+import Index from "./useReducer/useReducer-example-2";
 
 function App() {
   return (
---
+++ src/useReducer/useReducer-example-1.js
@@ -0,0 +1,66 @@
+//useReducer is  used to complex of state management 
+
+import React, { useReducer } from 'react'
+
+const reducer = (state, action) => {
+    if (action.type === "DELETE_PERSON") {
+        const newPersons = state.data.filter((eachPerson) => {
+            return eachPerson.id !== action.payload;
+        });
+        return {
+            ...state,
+            data: newPersons,
+            length: state.length - 1,
+
+        };
+    };
+    return state;
+};
+
+const Index = () => {
+
+    const initialState = {
+        data: [
+            { id: "1", firstName: "Phanindra", email: "phanindra@gmail.com" },
+            { id: "2", firstName: "srinu", email: "srinu@gmail.com" },
+        ],
+        length: 2,
+    };
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+    //reducer is modification of the state 
+
+    const handleDelete = (id) => {
+        //dispatch is modified the data 
+        //dispatch is internally connect to reducer
+        dispatch({
+            type: "DELETE_PERSON",
+            payload: id,
+        });
+    };
+
+    const handleEdit = (id) => {
+        dispatch({
+            type: "UPDATE_PERSON",
+            payload: id
+        });
+    };
+    return (
+        <div>
+            <h1>Current users length : {state.length}</h1>
+            {state.data.map((eachItem) => {
+                const { id, firstName, email } = eachItem;
+                return (
+                    <div key={id}>
+                        <h3>{firstName}</h3>
+                        <p>{email}</p>
+                        <button onClick={() => handleDelete(id)}>delete</button>
+                        <button onClick={() => handleEdit(id)}>Edit</button>
+                    </div>
+                );
+            })};
+        </div>
+    );
+};
+
+export default Index
